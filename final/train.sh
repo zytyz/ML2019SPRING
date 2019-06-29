0926c2d8b83394fbb28418d6feac0684aed4c466
zytyz <b06901007@ntu.edu.tw>
2019-06-30 00:20:44 +0800

    train.sh

--- final/train.sh
+++ final/train.sh
@@ -5,4 +5,5 @@ GT_VAL_PATH=$4
 GPU_NUM=$5
 python src/train/data_augment.py --size 224 --fold_A $HAZY_TRAIN_PATH --fold_B $GT_TRAIN_PATH --fold_AB TrainData
 python src/train/data_augment.py --size 224 --fold_A $HAZY_VAL_PATH --fold_B $GT_VAL_PATH --fold_AB TestData 
+mkdir logs
 python src/train/train.py --train TrainData --test TestData --cuda --gpus $GPU_NUM --loss MSE --tag model 
